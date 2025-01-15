7ab5779a2e9bfdd3bbb14308ad79ba7a140af5b0
victoriaeh <117381014+victoriaeh@users.noreply.github.com>
2025-01-15 16:37:41 -0500

    Edits

--- input_data_transform.py
+++ input_data_transform.py
@@ -1,17 +1,12 @@
 # transform input data: create one csv file for each site containing all
 # variables for that site - resulting in 16 new files
-
-import sys
-print(sys.executable)
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import csv
 
-directory = "/input_data"
+directory = "input_data"
 csv_list = [
     'air_avg.csv',
     'air_obs.csv',
@@ -27,28 +22,73 @@ csv_list = [
     'soil_avg_08.csv',
     'soil_avg_20.csv',
     'soil_avg_40.csv']
-# csv_dict = {
-#     'air_avg': 'air_avg.csv',
-#     'air_obs': 'air_obs.csv',
-#     'soil_obs_00': 'soil_obs_00.csv',
-#     'soil_obs_02': 'soil_obs_02.csv',
-#     'soil_obs_04': 'soil_obs_04.csv',
-#     'soil_obs_08': 'soil_obs_08.csv',
-#     'soil_obs_20': 'soil_obs_20.csv',
-#     'soil_obs_40': 'soil_obs_40.csv',
-#     'soil_avg_00': 'soil_avg_00.csv',
-#     'soil_avg_02': 'soil_avg_02.csv',
-#     'soil_avg_04': 'soil_avg_04.csv',
-#     'soil_avg_08': 'soil_avg_08.csv',
-#     'soil_avg_20': 'soil_avg_20.csv',
-#     'soil_avg_40': 'soil_avg_40.csv'}
 
+# initialize empty list for dataframes
+df_dict = {}
 
+# load csv files as dataframes
 for csv_file in csv_list:
-    filepath = os.path.join(directory, csv_list)
+    filepath = os.path.join(directory, csv_file)
     df_name = os.path.splitext(csv_file)[0] # basename of csv file
     dataframe = pd.read_csv(filepath, comment='#')
-    globals()[df_name] = dataframe
+    dataframe['week_start'] = pd.to_datetime(dataframe['week_start'])
+    df_dict[df_name] = dataframe
 
-    print(f'{df_name} dataframe created successfully')
+print(f'df_name dictionary created successfully')
 
+# print(df_dict)
+
+# initialize empty dictionary for site specific dataframes
+site_dict = {}
+
+# print(df_dict.keys())
+
+# get column names from df_list for naming site specific dataframes
+columns = df_dict['air_avg'].columns.difference(['week_start'])
+
+# create one df for each snotel site (organize data in df_list by snotel site
+# instead of by variable)
+for df_name, df in df_dict.items():
+    for column in df.columns:
+        if column != 'week_start':
+            # create a temporary dataframe with the selected column renaming
+            # the temp_df column based on the original df name
+            temp_df = df[[column]].rename(columns={column: 
+                                                          f"{df_name}"})
+
+            # create a dataframe if site_dict does not yet have one for 
+            # the selected column
+            if column not in site_dict:
+                site_dict[column] = temp_df
+            else:
+                # otherwise append the new data to the existing dataframe
+                site_dict[column] = pd.concat([site_dict[column], 
+                                              temp_df], axis=1)
+
+# add index column
+for column, dataframe in site_dict.items():
+    idx=0
+    week_number = range(1, len(dataframe) +1)
+    dataframe.insert(loc=idx, column='week_number', value=week_number)
+
+print(f'site_dict dictionary created successfully')
+
+# export dataframes in site_dict to csvs in new folder called site_data
+def export_df_to_csv(dictionary, dir_path='./'):
+    '''Exports dataframes in a dictionary to CSV files.
+    Args: 
+        dictionary: A dictionary where keys are dataframe names and
+        dir_path: The directory to save the CSV files. Defaults to current dir
+    '''
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    
+    for df_key, df in dictionary.items():
+        file_path = f"{dir_path}/{df_key}.csv"
+        df.to_csv(file_path, index=False)
+
+print(site_dict.keys())
+
+site_dir = 'site_data'
+
+export_df_to_csv(site_dict, site_dir)
